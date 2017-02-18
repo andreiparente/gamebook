e3592de5f3fbe21c94ca1cea6949039e7b073934
Andrei Leone Parente Silva <andreiparente@gmail.com>
2017-02-18 04:28:46 -0200

    Add files via upload

---
+++ gamebook/src/main/java/br/ufrrj/im/bigtrayenterprises/comp2/aa/Book.java
@@ -0,0 +1,72 @@
+package br.ufrrj.im.bigtrayenterprises.comp2.aa;
+
+import br.ufrrj.im.bigtrayenterprises.comp2.aa.Characters.Player;
+import br.ufrrj.im.bigtrayenterprises.comp2.aa.Choices.Choice;
+import br.ufrrj.im.bigtrayenterprises.comp2.aa.Events.BlankEvent;
+import br.ufrrj.im.bigtrayenterprises.comp2.aa.Events.Event;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by filipebraida on 31/05/16.
+ */
+public class Book {
+
+    public Book(String description, Event eventInitial, Player player) {
+        this.eventInitial = eventInitial;
+        this.description = description;
+        this.player = player;
+
+        this.resetHistory();
+    }
+
+    public void resetHistory() {
+        this.eventActually = this.eventInitial;
+    }
+
+    public String showHistory() {
+        return this.eventActually.getDescription();
+    }
+
+    public boolean isTheEnd() {
+        return this.eventActually.isEndEvent();
+    }
+
+    public String showHistoryBook() {
+        return this.description;
+    }
+
+    public boolean nextEvent(int number) {
+        Choice choice = this.selectChoice(number);
+
+        if (choice != null) {
+            choice.executeChoice(player);
+
+            if (player.isAlive()) {
+                this.eventActually = choice.getNextEvent();
+                this.eventActually.applyHistory(player);
+            } else {
+                Event gameOver = new BlankEvent(new ArrayList<Choice>(), "Game Over");
+                this.eventActually = gameOver;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public Choice selectChoice(int number) {
+        return this.eventActually.findChoice(number);
+    }
+
+    public Collection<Choice> nextEvents() {
+        return this.eventActually.getChoices();
+    }
+
+    private Event eventActually;
+    private Event eventInitial;
+    private String description;
+    private Player player;
+}
